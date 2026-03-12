875d7d0e0fee9ebc8c816f76035663a07bccca9a
rishithhhhh <rishith030605@gmail.com>
2026-03-13 00:12:09 +0530

    12/03

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,4 +5,39 @@ const getProducts = async (req, res) => {
   res.render("products/index", { products });
 };
 
-export {getProducts}
+const addProduct = async (req, res) => {
+  const product = req.body;
+  await productModel.create(product);
+  res.redirect("/products");
+};
+
+const addProductForm = async (req, res) => {
+  res.render("products/add");
+};
+
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  await productModel.findByIdAndDelete(id);
+  res.redirect("/products");
+};
+
+const editProductForm = async (req, res) => {
+  const id = req.params.id;
+  const product = await productModel.findOne({ _id: id });
+  res.render("products/edit", { product });
+};
+
+const saveProduct = async (req, res) => {
+  const id = req.params.id;
+  await productModel.findByIdAndUpdate(id, req.body);
+  res.redirect("/products")
+};
+
+export {
+  getProducts,
+  addProduct,
+  addProductForm,
+  deleteProduct,
+  editProductForm,
+  saveProduct
+};
---
+++ routes/homeRoute.js
@@ -0,0 +1,9 @@
+
+import homePage from "../controllers/homeController.js";
+import express from "express"
+
+const homeRouter = express.Router()
+
+homeRouter.get("/",homePage)
+
+export default homeRouter
